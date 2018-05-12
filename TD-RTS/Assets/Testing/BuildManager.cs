177eed6129d537fbcf0abaca4324e259a80a51a4
SwordOfWrynn <bubba.wiens@gmail.com>
2018-05-11 20:03:43 -0700

    Foo

---
+++ TD-RTS/Assets/Testing/BuildManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one BuildManager script in scene!");
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject standardTowerPrefab;
+
+    private GameObject towerToBuild;
+
+    void Start()
+    {
+        towerToBuild = standardTowerPrefab;
+    }
+
+    public GameObject GetTowerToBuild()
+    {
+        return towerToBuild;
+    }
+
+}
---
+++ TD-RTS/Assets/Testing/Node.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public Color hoverColor;
+
+    private GameObject tower;
+
+    private Renderer rend;
+    private Color startColor;
+
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseDown()
+    {
+        //if there is already a tower
+        if (tower != null)
+        {
+            Debug.Log("Can't build here");
+            return;
+        }
+
+        //Build a tower
+
+        GameObject towerToBuild = BuildManager.instance.GetTowerToBuild();
+        tower = Instantiate(towerToBuild, new Vector3(transform.position.x, transform.position.y, -1), transform.rotation);
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+
+}
