cbe46cff4b527fcc2e8e4cd26d5ab55b09c60788
SwordOfWrynn <bubba.wiens@gmail.com>
2018-05-18 12:25:42 -0700

    Foo

---
+++ TD-RTS/Assets/IsAvailable.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class IsAvailable : MonoBehaviour {
+
+    [Tooltip("Use var format e.g. standardTower")]
+    public string towerType;
+    public Sprite lockedSprite;
+    public Image image;
+
+    private Sprite startSprite;
+    private Color startColor;
+    private TowersAvailable towersAvailable;
+
+    // Use this for initialization
+    void Start()
+    {
+        startSprite = image.sprite;
+        startColor = image.color;
+        towersAvailable = TowersAvailable.instance;
+        CheckAvailable();
+    }
+
+    public void CheckAvailable () {
+        Debug.Log("Checking Availability");
+        if (towerType == "standardTower")
+        {
+            if (!towersAvailable.standardTower)
+            {
+                GetComponent<Button>().interactable = false;
+                image.sprite = lockedSprite;
+                image.color = Color.white;
+            }
+            else
+            {
+                GetComponent<Button>().interactable = true;
+                image.sprite = startSprite;
+                image.color = startColor;
+            }
+        }
+        if (towerType == "missileTower")
+        {
+            if (!towersAvailable.missileTower)
+            {
+                GetComponent<Button>().interactable = false;
+                image.sprite = lockedSprite;
+                image.color = Color.white;
+            }
+            else
+            {
+                GetComponent<Button>().interactable = true;
+                image.sprite = startSprite;
+                image.color = startColor;
+            }
+        }
+        if (towerType == "laserTower")
+        {
+            if (!towersAvailable.laserTower)
+            {
+                GetComponent<Button>().interactable = false;
+                image.sprite = lockedSprite;
+                image.color = Color.white;
+            }
+            else
+            {
+                GetComponent<Button>().interactable = true;
+                image.sprite = startSprite;
+                image.color = startColor;
+            }
+        }
+    }
+}
---
+++ TD-RTS/Assets/Testing/TowersAvailable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowersAvailable : MonoBehaviour {
+
+    #region Singleton management
+    public static TowersAvailable instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one TowerAvailable script in scene!");
+            return;
+        }
+        instance = this;
+    }
+    #endregion
+
+    public bool standardTower;
+    public bool missileTower;
+    public bool laserTower;
+
+    public GameObject[] buildingButtons;
+    public List<IsAvailable> isAvailableList;
+
+    // Use this for initialization
+    void Start () {
+        buildingButtons = GameObject.FindGameObjectsWithTag("BuildingButtons");
+        IsAvailable isAvailable;
+        for (int i = 0; i < buildingButtons.Length; i++)
+        {
+            isAvailable = buildingButtons[i].GetComponent<IsAvailable>();
+            isAvailableList.Add(isAvailable);
+        }
+    }
+
+    public void CallCheckAvailable ()
+    {
+        for (int i = 0; i < isAvailableList.Count; i++)
+        {
+            isAvailableList[i].CheckAvailable();
+        }
+    }
+}
