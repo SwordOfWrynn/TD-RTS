7e161f96b38aa655b642d55a01a327000eec4f8b
SwordOfWrynn <bubba.wiens@gmail.com>
2018-05-22 13:59:08 -0700

    Foo

---
+++ TD-RTS/Assets/Testing/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject pauseUI;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            Toggle();
+        }
+	}
+
+    public void Toggle()
+    {
+        pauseUI.SetActive(!pauseUI.activeSelf);
+
+        if (pauseUI.activeSelf)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void MainMenu()
+    {
+
+    }
+
+}
