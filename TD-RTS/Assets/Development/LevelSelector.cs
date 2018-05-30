0dfca8512ce31b3731f9d0e9c6d498b94b179fef
SwordOfWrynn <bubba.wiens@gmail.com>
2018-05-30 14:00:07 -0700

    Foo

---
+++ TD-RTS/Assets/Development/LevelSelector.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class LevelSelector : MonoBehaviour {
+
+    public SceneFader sceneFader;
+
+    public void Select(string _levelName)
+    {
+        sceneFader.FadeTo(_levelName);
+    }
+    
+}
