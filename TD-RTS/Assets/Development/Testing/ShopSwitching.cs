ce55ea22c96f5198caa586d58008302ecc7a1457
SwordOfWrynn <bubba.wiens@gmail.com>
2018-06-01 13:52:53 -0700

    Foo

---
+++ TD-RTS/Assets/Development/Testing/ShopSwitching.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopSwitching : MonoBehaviour {
+
+    public GameObject towerShop;
+    public GameObject baseShop;
+
+	public void TurnOnTowerShop()
+    {
+        towerShop.SetActive(true);
+        baseShop.SetActive(false);
+    }
+
+    public void TurnOnBaseShop()
+    {
+        baseShop.SetActive(true);
+        towerShop.SetActive(false);
+    }
+
+}
